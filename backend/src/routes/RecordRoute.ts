d6d559615bc2af8c37ac2eb199e925857fd89c62
Vuong Hoang <hoangasphalt89@gmail.com>
2025-04-19 15:51:46 +0700

    deploy on vps

--- backend/src/routes/RecordRoute.ts
+++ backend/src/routes/RecordRoute.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import RecordController from "../controllers/RecordController";
-import { jwtCheck, jwtParse } from "../middleware/auth";
+// import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
-router.get("/", jwtCheck, jwtParse, RecordController.getRecord); // Lấy danh sách Record
-router.post("/", jwtCheck, jwtParse, RecordController.addRecord); // Thêm Record
-router.put("/:id", jwtCheck, jwtParse, RecordController.editRecord); // Sửa Record
-router.delete("/:id", jwtCheck, jwtParse, RecordController.removeRecord); // Xóa Record
+router.get("/", RecordController.getRecord); // Lấy danh sách Record
+router.post("/", RecordController.addRecord); // Thêm Record
+router.put("/:id", RecordController.editRecord); // Sửa Record
+router.delete("/:id", RecordController.removeRecord); // Xóa Record
 
 export default router;

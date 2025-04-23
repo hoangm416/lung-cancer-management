6a0febcc56d145ba158ce44d1477b97b5d22f0ab
dta181104 <165494716+dta181104@users.noreply.github.com>
2025-04-23 18:30:38 +0700

    them bieu do gender va age

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,49 @@ import HistogramChart from "@/components/HistogramChart";
 import { useGetRecord } from "@/api/LungRecordApi";
 
 const HomePage = () => {
-  // const { records, isLoading } = useGetRecord() 
+  const { records, isLoading } = useGetRecord();
+  if (isLoading) return <div>Đang tải dữ liệu...</div>;
+
+// Xử lý dữ liệu biểu đồ tỷ lệ giới tính
+  const genderCounts = records.reduce(
+    (acc, record) => {
+      const gender = record.gender?.toLowerCase();
+      if (gender === "male" || gender === "nam") {
+        acc.male++;
+      } else if (gender === "female" || gender === "nữ") {
+        acc.female++;
+      } else {
+        acc.undefined++;
+      }      
+      return acc;
+    },
+    { male: 0, female: 0, undefined:0 }
+  );
+  
+  const gendertData = [
+    { name: "Nam", value: genderCounts.male },
+    { name: "Nữ", value: genderCounts.female },
+    { name: "Không xác định", value: genderCounts.undefined },
+  ];
+
+  // Xử lý dữ liệu biểu đồ độ tuổi
+  const ageGroups = new Map<string, number>();
+  for (const record of records) {
+    const age = Number(record.age_at_index);
+    if (isNaN(age)) continue;
+
+    const bucket = `${Math.floor(age / 10) * 10}-${Math.floor(age / 10) * 10 + 9}`;
+    ageGroups.set(bucket, (ageGroups.get(bucket) || 0) + 1);
+  }
+
+  const ageData = Array.from(ageGroups.entries())
+    .map(([ageRange, count]) => ({ ageRange, count }))
+    .sort((a, b) => {
+      // Sắp xếp theo độ tuổi tăng dần
+      const aStart = parseInt(a.ageRange.split("-")[0]);
+      const bStart = parseInt(b.ageRange.split("-")[0]);
+      return aStart - bStart;
+    });
 
   // const genderData = records.reduce((acc, record) => {
   //   const genderGroup = getGenderGroup(record.gender); 
@@ -38,13 +80,13 @@ const HomePage = () => {
         {/* Cột 1: Biểu đồ PieChart */}
         <div className="flex flex-col items-center justify-center">
           <h2 className="text-xl font-semibold mb-4">Tỷ lệ giới tính</h2>
-          <PieChartComponent />
+          <PieChartComponent data={gendertData}/>
         </div>
 
         {/* Cột 2: Biểu đồ HistogramChart */}
         <div className="flex flex-col items-center justify-center">
           <h2 className="text-xl font-semibold mb-4">Phân bố độ tuổi</h2>
-          <HistogramChart />
+          <HistogramChart data={ageData}/>
         </div>
       </div>
     </div>

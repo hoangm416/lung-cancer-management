5ddb3c7b8f2841b4ebd41d653c4fc2c4eb00c415
dta181104 <165494716+dta181104@users.noreply.github.com>
2025-05-16 17:26:36 +0700

    them trang huong dan

---
+++ frontend/src/pages/GuidePage.tsx
@@ -0,0 +1,16 @@
+
+
+const GuidePage = () => {
+  return (
+    <div className="flex flex-col gap-10">
+        <div className="text-left">
+        <span className="font-bold text-3xl">
+            Hướng dẫn sử dụng trang web
+        </span>
+        </div>
+    </div>
+  );
+  
+}
+
+export default GuidePage;
